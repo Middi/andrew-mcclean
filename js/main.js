89075e629465fa53a876fa72242df83c77c8af2a
“Richard Middleton” <Richard@richardmiddleton.me>
2017-10-26 14:32:58 +0100

    contact page and services page added

--- js/main.js
+++ js/main.js
@@ -1,43 +1,42 @@
 $(document).ready(function () {
-    
-// ===========================
-// Mobile Nav
-// ===========================
 
-$('#hamburger').click(function () {
-if ($('nav').hasClass('hidden')) {
-     $('nav').slideDown( "slow", function() {
-        $('nav').removeClass('hidden');
-     });
-}
-else {
-    $('nav').slideUp( "slow", function() {
-        $('nav').addClass('hidden');
-    });
-}
-});
+    // ===========================
+    // Mobile Nav
+    // ===========================
 
-      
-// ===========================
-// Smooth Scrolling
-// ===========================
-
-$('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-
-            $('#menu').slideUp( "slow", function() {
-            $('#menu').addClass('hidden');
-            
+    $('#hamburger').click(function () {
+        if ($('nav').hasClass('hidden')) {
+            $('nav').slideDown("slow", function () {
+                $('nav').removeClass('hidden');
             });
-            return false;
+        } else {
+            $('nav').slideUp("slow", function () {
+                $('nav').addClass('hidden');
+            });
         }
-    }
-});      
+    });
+
+
+    // ===========================
+    // Smooth Scrolling
+    // ===========================
+
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+
+                $('#menu').slideUp("slow", function () {
+                    $('#menu').addClass('hidden');
+
+                });
+                return false;
+            }
+        }
+    });
 });
